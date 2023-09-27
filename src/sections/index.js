d9c446888e006b6decc792914937e248217d7977
aguiarHugo <aguiarhugofx@gmail.com>
2023-09-27 13:44:32 -0300

    exported new component

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,5 +1,6 @@
 import About from './About'
 import Hero from './Hero'
 import Navbar from './Navbar'
+import Contact from './Contact'
 
-export { About, Hero, Navbar }
+export { About, Hero, Navbar, Contact }
