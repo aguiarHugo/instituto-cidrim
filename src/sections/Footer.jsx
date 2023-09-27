66dd442aeb66872dbc30b326c8eb9f5f017746a4
aguiarHugo <aguiarhugofx@gmail.com>
2023-09-27 13:47:57 -0300

    fontsize

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-        <div className='flex justify-between px-4 text-secondary text-[12px]'>
+        <div className='flex justify-between px-4 text-secondary text-[10px] md:text-[12px]'>
            <p>2023 &copy;Instituto Cidrim, todos os direitos reservados.</p>
            <a href="https://hugoaguiar.vercel.app/" target='_blank'>Desenvolvido por: Hugo Aguiar </a>
         </div>
