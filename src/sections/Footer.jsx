a7bc23a3692abaf4526c38785c68cfb6cfe04db4
aguiarHugo <aguiarhugofx@gmail.com>
2023-09-27 13:44:18 -0300

    Created and Styled

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className='flex justify-between px-4 text-secondary text-[12px]'>
+           <p>2023 &copy;Instituto Cidrim, todos os direitos reservados.</p>
+           <a href="https://hugoaguiar.vercel.app/" target='_blank'>Desenvolvido por: Hugo Aguiar </a>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
